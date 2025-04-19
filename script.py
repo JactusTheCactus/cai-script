7adc8ed8bcb10b888933ca1bff1b37f994b2ca4b
Devin <threateninglyamusing@gmail.com>
2025-04-18 19:50:46 -0700

    -

--- script.py
+++ script.py
@@ -1,31 +1,27 @@
 import re
 
-chatLog = 'logs/Kristina_tavern_Chat.jsonl'
-user = r'Devin'
+chatLogs = ['Kristina_tavern_Chat.jsonl']
 
-with open(chatLog,'r',encoding='utf-8') as f:
-    log = f.read()
+def logChat(chatLog,user):
+    with open(f'logs/{chatLog}','r',encoding='utf-8') as f:
+        log = f.read()
+    def regex(item,match,replace):
+        return re.sub(match,replace,item)
+    regexList = [
+        [r'("(?:user_)?name":")You(")',r'\1' + user + r'\2'],
+        [r'\\"',r'"'],
+        [r'\{"user_name":"(.*?)","character_name":"(.*?)".*?\}',r'# Chat Between `\1` & `\2`:\n'],
+        [r'\{"name":"(.*?)","is_user":.*?,"is_name":.*?,"send_date":.*?,"mes":"(.*?)"\}',r'## `\1`:\n\2\n'],
+        [r'\\n',r'\n'],
+        [r'OOC: __(.*?)__',r'\1']
+    ]
+    for i in regexList:
+        log = regex(log,i[0],i[1])
+    log = re.sub(r'',r'',log)
+    logName = re.sub(r'(.*)_tavern_Chat(.*?).jsonl',r'\1\2',chatLog)
+    print(logName)
+    with open(f'chats/{logName}.md','w',encoding='utf-8') as f:
+        f.write(log)
 
-def regex(item,match,replace):
-    return re.sub(match,replace,item)
-
-regexList = [
-    [r'("(?:user_)?name":")You(")',r'\1' + user + r'\2'],
-    [r'\\"',r'"'],
-    [r'\{"user_name":"(.*?)","character_name":"(.*?)".*?\}',r'# Chat Between `\1` & `\2`:\n'],
-    [r'\{"name":"(.*?)","is_user":.*?,"is_name":.*?,"send_date":.*?,"mes":"(.*?)"\}',r'## `\1`:\n\2\n'],
-    [r'\\n',r'\n'],
-    [r'OOC: __(.*?)__',r'\1']
-]
-
-for i in regexList:
-    log = regex(log,i[0],i[1])
-
-log = re.sub(r'',r'',log)
-
-logName = re.sub(r'(.*)_tavern_Chat(.*?).jsonl',r'\1\2',chatLog)
-
-print(logName)
-
-with open(f'{re.sub(r'logs/',r'chats/',logName)}.md','w',encoding='utf-8') as f:
-    f.write(log)
+for i in chatLogs:
+    logChat(i,r'Devin')
