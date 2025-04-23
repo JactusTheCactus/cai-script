cc24f842d42f584fc99505ba46b88d8533608689
JactusTheCactus <threateninglyamusing@gmail.com>
2025-04-23 01:41:45 +0000

    -

--- scripts/lore.py
+++ scripts/lore.py
@@ -18,7 +18,7 @@ def lore():
             lore = json.load(f)
         for i in lore['entries']:
             if i['enabled'] == True:
-                output += f'> # {i['name']}\n> {i['entry']}\n\n'
+                output += f"> # {i['name']}\n> {i['entry']}\n\n"
         with open(outputFile,'w',encoding='utf-8') as f:
             f.write(output)
         with open('log.md','a',encoding='utf-8') as f:
