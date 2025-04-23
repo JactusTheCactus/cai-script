d41df870af1561fb60bdc99132fa4e1968ed55e4
JactusTheCactus <threateninglyamusing@gmail.com>
2025-04-23 22:49:14 +0000

    -

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Instructions
-    1. Run `script.py` to build readable versions of Chat Logs & Lore Books.
-    1. file names follow a specific format:
-        - `Chats`: 
-            - `User&MainCharacter-##*optional.json/jsonl`
-            - example: `Johnny&Jessica-03.json`
-        - `Lore Books`:
-            - `topic.json`
-            - example: `Lakedaimon.json`
+1. Run `script.py` to build readable versions of Chat Logs & Lore Books.
+1. file names follow a specific format:
+    - `Chats`: 
+        - `User&MainCharacter-##*optional.json/jsonl`
+        - example: `Johnny&Jessica-03.json`
+    - `Lore Books`:
+        - `topic.json`
+        - example: `Lakedaimon.json`
--- scripts/script.py
+++ scripts/script.py
@@ -51,14 +51,13 @@ def main():
     chat()
     lore()
     README = """# Instructions
-    1. Run `script.py` to build readable versions of Chat Logs & Lore Books.
-    1. file names follow a specific format:
-        - `Chats`: 
-            - `User&MainCharacter-##*optional.json/jsonl`
-            - example: `Johnny&Jessica-03.json`
-        - `Lore Books`:
-            - `topic.json`
-            - example: `Lakedaimon.json`"""
+file names follow a specific format:
+    - `Chats`: 
+        - `User&MainCharacter-##*optional.json/jsonl`
+        - example: `Johnny&Jessica-03.json`
+    - `Lore Books`:
+        - `topic.json`
+        - example: `Lakedaimon.json`"""
     print("Generating README.md")
     with open('README.md','w',encoding='utf-8') as f:
         f.write(README)
